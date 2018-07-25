9bfefd5031c598473ae0af9d3b207812e9d4a4a0
matjukov-nikolaj <blatnoi.kolyan@yandex.ru>
2018-07-25 15:48:07 +0300

    clean folder

--- src/main/java/com/capitalone/dashboard/repository/CheckMarxCollectorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.capitalone.dashboard.repository;
-
-import com.capitalone.dashboard.model.CheckMarxCollector;
-
-public interface CheckMarxCollectorRepository extends BaseCollectorRepository<CheckMarxCollector> {
-}
